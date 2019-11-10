a399b16ce0279b1760f9da361f9773d80750cc1f
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-11-10 00:05:40 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,21 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
 
-end
+  def key_for_min_value(name_hash)
+    def key_for_min_value(name_hash)
+    min = nil
+    key = nil
+    name_hash.collect do |name, age|
+      if min == nil || age < min
+        min = age
+        key = name
+      else
+        if min > age
+          min = age
+          key = name
+        end
+      end
+    end
+    key
+  end
